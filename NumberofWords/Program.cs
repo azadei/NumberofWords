0f09df70457bcfbba905eead76e61019771156a1
azadei <azadei@code.edu.az>
2023-07-10 15:54:04 +0400

    1

---
+++ NumberofWords/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class CountWords
+{
+    public static void Main()
+    {
+        String a = "CodeAcademy is a good place to study";
+        int wordCount = 0;
+
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            
+            if (a[i] == ' ' && Char.IsLetter(a[i + 1]) && (i > 0))
+            {
+                wordCount++;
+            }
+        }
+         
+        wordCount++;
+
+        
+        Console.WriteLine("Total number of words in the string is: " + wordCount);
+    }  
+}
